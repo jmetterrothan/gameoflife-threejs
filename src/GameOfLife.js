66d6892abaa6fc075610427d3a8f6e47cea4b8de
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2018-10-26 00:59:11 +0200

    Implemented game of life algo

--- src/GameOfLife.js
+++ src/GameOfLife.js
@@ -1,5 +1,12 @@
 import THREE from 'three';
 
+import utility from './utility';
+
+/*
+* Algorithm based on the following wikibooks resource :
+* https://en.wikibooks.org/wiki/Algorithm_Implementation/Simulation/Conway%27s_Game_of_Life
+*/
+
 class GameOfLife {
     constructor(size, rows, cols) {
         this.size = size;
@@ -7,38 +14,184 @@ class GameOfLife {
         this.rows = rows;
 
         this.cells = [];
+        this.temp = [];
+
+        this.running = false;
+
+        this.selectedShape = 0;
+        this.speed = 250;
+
+        this.tick = 0;
+        this.timer = null;
     }
 
     init(scene) {
-        const group = new THREE.Group();
+        // build the grid
+        const grid = new THREE.Group();
 
-        const geometry = new THREE.BoxBufferGeometry(this.size, this.size, this.size);
-        const material = new THREE.MeshPhongMaterial({ color: 0xffff00 , shininess: 50 });
-        const mesh = new THREE.Mesh(geometry, material);
+        // shared geometry
+        const geometry = new THREE.PlaneBufferGeometry(this.size, this.size, this.size);
 
         for(let row = 0; row < this.rows; row++) {
             for(let col = 0; col < this.cols; col++) {
                 if(col == 0) {
                     this.cells[row] = [];
+                    this.temp[row] = [];
                 }
 
-                const cell = mesh.clone();
-
+                const cell = new THREE.Mesh(geometry, GameOfLife.CELL_MATERIAL_DEAD);
+     
+                // init cell
                 cell.position.set(col * this.size, row * this.size, 0);
-                //cell.visible = false;
+                cell.coords = { row: row, col: col };
+                cell.alive = false;
+                cell.material = GameOfLife.CELL_MATERIAL_DEAD;
 
-                group.add(cell);
+                grid.add(cell);
+
                 this.cells[row][col] = cell;
+                this.temp[row][col] = false;
             } 
         }
-        group.position.set(-this.cols * this.size / 2, -this.rows * this.size / 2, 0);
 
-        scene.add(group);
+        // centers the grid in (0, 0, 0)
+        grid.position.set(-this.cols * this.size / 2, -this.rows * this.size / 2, 0);
+
+        scene.add(grid);
     }
 
-    update() {
+    update(delta) {
+        // simulate the game of life
+        if (this.running) {
+            if (this.timer === null || delta >= this.timer) {
+                // simulate a step
+                this.step();
+                this.timer = delta + this.speed;
+            }
+        }
+    }
+
+    step() {
+        for(let row = 0; row < this.rows; row++) {
+            for(let col = 0; col < this.cols; col++) {
+                this.setCellStatus(row, col, this.getCellStatus(row, col));
+            }
+        }
+        for(let row = 0; row < this.rows; row++) {
+            for(let col = 0; col < this.cols; col++) {
+                this.temp[row][col] = this.cells[row][col].alive;
+            }
+        }
+    }
+
+    /**
+     * Count the number of neighbours of a cell
+     * @param {number} row 
+     * @param {number} col 
+     */
+    getNeighbourCount(row, col) {
+        let n = 0;
+
+        if (this.temp[utility.mod(row + 1, this.rows)][col]) n++;
+        if (this.temp[utility.mod(row - 1, this.rows)][col]) n++;
+
+        if (this.temp[utility.mod(row - 1, this.rows)][utility.mod(col - 1, this.cols)]) n++;
+        if (this.temp[utility.mod(row - 1, this.rows)][utility.mod(col + 1, this.cols)]) n++;
+
+        if (this.temp[utility.mod(row + 1, this.rows)][utility.mod(col + 1, this.cols)]) n++;
+        if (this.temp[utility.mod(row + 1, this.rows)][utility.mod(col - 1, this.cols)]) n++;
+
+        if (this.temp[row][utility.mod(col + 1, this.cols)]) n++;
+        if (this.temp[row][utility.mod(col - 1, this.cols)]) n++;
         
+        return n;
+    }
+
+    /**
+     * Return the current state of a cell (alive or not)
+     * based on the rules of Conway's game of life
+     * @param {number} row 
+     * @param {number} col 
+     * @return boolean
+     */
+    getCellStatus(row, col) {
+        const n = this.getNeighbourCount(row, col);
+
+        return n === 3 || (this.temp[row][col] && n === 2);
+    }
+
+    resume() {
+        this.running = true;
+    }
+
+    pause() {
+        this.running = false;
+    }
+
+    /**
+     * Change a cell status (dead or alive)
+     * @param {number} row 
+     * @param {number} col 
+     * @param {bool} status 
+     */
+    setCellStatus(row, col, status) {
+        const cell = this.cells[row][col];
+
+        if (!status) {
+            cell.material = GameOfLife.CELL_MATERIAL_DEAD;
+        } else {
+            cell.material = GameOfLife.CELL_MATERIAL_ALIVE;
+        }
+
+        cell.alive = status;
+    }
+
+    /**
+     * Change the speed of the simulation
+     * @param {number} speed 
+     */
+    setSpeed(speed) {
+        this.speed = parseInt(speed, 10);
+        if (this.speed < 25) {
+            this.speed = 25;
+        }
+        this.timer = null;
+    }
+
+    selectShape(index) {
+        this.selectedShape = index;
+    }
+
+    /**
+     * Change the status of all cells randomly
+     */
+    randomize() {
+        for(let row = 0; row < this.rows; row++) {
+            for(let col = 0; col < this.cols; col++) {
+                const alive = Math.random() > 0.5;
+                this.setCellStatus(row, col, alive);
+                this.temp[row][col] = alive;
+            }
+        }
+    }
+
+    /**
+     * Set all cells dead
+     */
+    reset() {
+        for(let row = 0; row < this.rows; row++) {
+            for(let col = 0; col < this.cols; col++) {
+                this.setCellStatus(row, col, false);
+                this.temp[row][col] = false;
+            }
+        }
     }
 }
+
+GameOfLife.CELL_MATERIAL_ALIVE = new THREE.MeshPhongMaterial({ color: 0xffc300 , shininess: 35 });
+GameOfLife.CELL_MATERIAL_ALIVE.side = THREE.DoubleSide;
+
+GameOfLife.CELL_MATERIAL_DEAD = new THREE.MeshPhongMaterial({ color: 0x581845 , shininess: 0 });
+GameOfLife.CELL_MATERIAL_DEAD.side = THREE.DoubleSide;
 
 export default GameOfLife;
